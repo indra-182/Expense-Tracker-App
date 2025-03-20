10ef1b0e9a0f5d5a8db6d11450b465783469e21d
indra-182 <mahadiindra2@gmail.com>
2025-03-21 02:32:23 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +26,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning className="dark">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <RootProviders>{children}</RootProviders>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ components/Logo.tsx
@@ -0,0 +1,14 @@
+import { BadgeDollarSign } from "lucide-react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <BadgeDollarSign className="stroke h-11 w-11 stroke-amber-500 stroke[1.5]" />
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Expense Tracker
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
